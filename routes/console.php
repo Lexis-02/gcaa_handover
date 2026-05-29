8031bce0f7a3bab145fe5a68f84afec8d086d9a1
Lexis-02 <lexishorlali02@gmail.com>
2026-05-29 08:42:12 -0700

    feat: register daily register summary schedule in console routes

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:send-daily-register-summary')->dailyAt('17:00');
+Schedule::command('app:send-daily-register-summary')->dailyAt('16:30');
